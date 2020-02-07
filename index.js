810e7f2b64c1df0273ac424787b894d9b75160d0
Peyton-Cysewski <pcysewski12@gmail.com>
2020-02-07 13:34:03 -0800

    To clean up nesting

--- index.js
+++ index.js
@@ -3,30 +3,30 @@
 function title_prompt() { //Tests prompts
 
 //Do you like Planes?
-var num = prompt('Do you like airplanes (1 = yes, 0 = no)?');
-var words;
-var id;
+    var num = prompt('Do you like airplanes (1 = yes, 0 = no)?');
+    var words;
+    var id;
 
-if (num >= 1) {
-    id = 'id="good"';
-    words = 'Welcome to Airplanes.com';
-} else if (num == 0) {
-    id = 'id="bad"';
-    words = 'You can leave now';
-} else {
-    id = 'id="ugly"';
-    words = 'Dude not cool, don\'t be a troll';
-}
+    if (num >= 1) {
+        id = 'id="good"';
+        words = 'Welcome to Airplanes.com';
+    } else if (num == 0) {
+        id = 'id="bad"';
+        words = 'You can leave now';
+    } else {
+        id = 'id="ugly"';
+        words = 'Dude not cool, don\'t be a troll';
+    };
 
-document.write('<h1 ' + id + '>' + words + '</h1>');
+    document.write('<h1 ' + id + '>' + words + '</h1>');
 }
 
 function run_alert () { //Tests alerts
-alert('Be Prepared for pure, unadulterated awesomeness!!!');
+    alert('Be Prepared for pure, unadulterated awesomeness!!!');
 }
 
 function run_confirm () { //Tests confirm
-confirm('Are you ready?');
+    confirm('Are you ready?');
 }
 
 function filler() { //Makes a lit of #'s to test function ability
@@ -41,6 +41,6 @@ function filler() { //Makes a lit of #'s to test function ability
     
     for (var i = 0; i < answer; i++) { //makes the string of characters to desired length
         total = total + fill;
-    }
+    };
     return total;
 }
